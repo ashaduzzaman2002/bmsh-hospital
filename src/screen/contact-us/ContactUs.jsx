a7dbd8321603d0985b7b1a2d5bd3b0c26d5a7bae
ProgrammerAlok <alokpaul5584@gmail.com>
2023-10-07 10:29:01 +0530

    contact-component added

--- src/screen/contact-us/ContactUs.jsx
+++ src/screen/contact-us/ContactUs.jsx
@@ -3,12 +3,14 @@ import "./contactus.css";
 import Layout from "../../layout/Layout";
 import BannerSecondary from "../../components/Banner/BannerSecondary";
 import Heading from "../../components/heading/Heading";
+import ContactComponent from "../../components/contact-us/ContactComponent";
 
 const ContactUs = () => {
   return (
     <Layout title={"Contact Us"} activeLink={"Contact Us"}>
       <BannerSecondary title={"Contact Us"} />
       <Heading title={"Contact Us"} />
+      <ContactComponent/>
     </Layout>
   );
 };
