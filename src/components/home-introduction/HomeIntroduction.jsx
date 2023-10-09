8e3f1e839278aba9ffd2cd667c46fde56c0fa48b
ashaduzzaman2002 <ashaduzzaman2002@gmail.com>
2023-10-09 23:35:39 +0530

    home intro

--- src/components/home-introduction/HomeIntroduction.jsx
+++ src/components/home-introduction/HomeIntroduction.jsx
@@ -15,76 +15,99 @@ const HomeIntroduction = () => {
 
       <div className="intro-bottom-container">
         <div>
-          <div className="indro-text-box">
-            <div className="intro-text-inner">
-              <div>
-                <img src="/images/step-initial.svg" alt="" />
-              </div>
+          <div>
 
-              <div>
-                <div>
-                  <h2 className="mb-0">Mission</h2>
-                  <p className="mb-0">
-                    To Provide Best Health Care At Affordable Price.
-                  </p>
-                </div>
-
-                <div>
-                  <h2 className="mb-0">Vision</h2>
-                  <p className="mb-0">
-                    To Provide World Class Health Services To All.
-                  </p>
-                </div>
-
-                <div>
-                  <h2 className="mb-0">Philosophy</h2>
-                  <p className="mb-0">
-                    Holistic, coordinated, and effective patient care based.
-                  </p>
-                </div>
-              </div>
+            
+            <div className="indro-text-box">
+            <div className="single-dot">
+              <img src="/images/single-dot.svg" alt="" />
             </div>
+              <div className="intro-text-inner">
+                <div>
+                  <img src="/images/step-initial.svg" alt="" />
+                </div>
 
-            <div className="intro-text-inner">
-              <div>
-                <img src="/images/step-end.svg" alt="" />
+                <div>
+                  <div>
+                    <h2 className="mb-0">Mission</h2>
+                    <p className="mb-0">
+                      To Provide Best Health Care At Affordable Price.
+                    </p>
+                  </div>
+
+                  <div>
+                    <h2 className="mb-0">Vision</h2>
+                    <p className="mb-0">
+                      To Provide World Class Health Services To All.
+                    </p>
+                  </div>
+
+                  <div>
+                    <h2 className="mb-0">Philosophy</h2>
+                    <p className="mb-0">
+                      Holistic, coordinated, and effective patient care based.
+                    </p>
+                  </div>
+                </div>
               </div>
 
-              <div>
+              <div className="intro-text-inner">
                 <div>
-                  <h2 className="mb-0">Values</h2>
-                  <p className="mb-0">
-                    The unique position of the family in society.
-                  </p>
+                  <img src="/images/step-end.svg" alt="" />
                 </div>
 
                 <div>
-                  <h2 className="mb-0">Belife</h2>
-                  <p className="mb-0">
-                    We Believe our role is to help you Maximize your health.
-                  </p>
-                </div>
+                  <div>
+                    <h2 className="mb-0">Values</h2>
+                    <p className="mb-0">
+                      The unique position of the family in society.
+                    </p>
+                  </div>
 
-                <div>
-                  <h2 className="mb-0">Quality Assurance</h2>
-                  <p className="mb-0">
-                    We will aggressively implement processes that advance.
-                  </p>
+                  <div>
+                    <h2 className="mb-0">Belife</h2>
+                    <p className="mb-0">
+                      We Believe our role is to help you Maximize your health.
+                    </p>
+                  </div>
+
+                  <div>
+                    <h2 className="mb-0">Quality Assurance</h2>
+                    <p className="mb-0">
+                      We will aggressively implement processes that advance.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
 
           <div className="intro-image-1">
-          <img className="intro-dot-left" src="/images/dot.svg" alt="" />
-            <img className="intro-left-img" src="/images/intro-img-1.png" alt="" />
+            <img className="intro-dot-left" src="/images/dot.svg" alt="" />
+            <img
+              className="intro-left-img"
+              src="/images/intro-img-1.png"
+              alt=""
+            />
+
+            <div className="intro-img">
+              <img className="intro-dot-right" src="/images/dot.svg" alt="" />
+              <img
+                className="intro-right-img"
+                src="/images/intro-img-2.png"
+                alt=""
+              />
+            </div>
           </div>
         </div>
 
         <div className="intro-second-img">
-
           <img className="intro-dot-right" src="/images/dot.svg" alt="" />
-          <img className="intro-right-img" src="/images/intro-img-2.png" alt="" />
+          <img
+            className="intro-right-img"
+            src="/images/intro-img-2.png"
+            alt=""
+          />
         </div>
       </div>
     </div>
